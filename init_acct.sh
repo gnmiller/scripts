49087628ba1d0389324f3ecd24530b10a658e6f6
Greg Miller <gnmiller@mtu.edu>
2016-12-23 07:05:14 -0800

    updated to init_acct.sh
    grabs vimrc and bashrc now too

--- init_acct.sh
+++ init_acct.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+wget https://raw.githubusercontent.com/gnmiller/scraps/master/.bashrc --no-check-certificate
+wget https://raw.githubusercontent.com/gnmiller/scraps/master/.vimrc --no-check-certificate
 mkdir ~/.ssh/ -m g-rwx,o-rwx
 touch ~/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDISWo3i9IJBBpnBD+j+svaUk0oDyd/b0gMOdzRWk+A71UiQkEAwrNnGzX7HYuQ7PUejjuYdUHSRaIJpvo0F/Mxv1jFA9TPNcKlS07ngEwM7qHyXPBtE4WDFd3lUpOxfEaPg4YBfrOcy40DcXwUMX0I3am1Z4OsXU7s3OK5bju1IAU8Xdkxk1oKjhHWMMHsUgvDqM/4yixRvD/X7/R1u6aFVnnxbhUygVHEhWNI5xpMN4gMVAq40I7y3kN4kPEMYiwjyGTX2h32Vn+uJtfKFsSrB4meHVxdJLUdWjXuB+ipeaNkKD8EUW3Z6ZFepzZ4fKadDUij7f9aprbNMYc76i9x greg.miller@gtbgremilles60a.tdi.corp.jwt.com" >> ~/.ssh/authorized_keys
