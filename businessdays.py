53f396d4f0bbba41d9e260d629541d084426f485
btcraig <gnmiller@users.noreply.github.com>
2018-03-19 15:37:17 -0400

    slightly better documentation

--- businessdays.py
+++ businessdays.py
@@ -3,7 +3,7 @@ from datetime import date, timedelta as td
 from dateutil.parser import parse
 import argparse
 
-parser = argparse.ArgumentParser( description="Prints the number of weekdays between two dates." )
+parser = argparse.ArgumentParser( description="Prints the number of weekdays between two dates. Uses the dateutil library to parse the input. Should be able to accept most standard date formats." )
 parser.add_argument( "start", action="store", help="the date to start counting from." )
 parser.add_argument( "end", action="store", help="The date to count to." )
 parser.add_argument( "-i", "--inverse", action="store_true", help="Perform the inverse action and print the number of weekends" )
